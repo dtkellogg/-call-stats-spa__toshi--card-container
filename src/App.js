c43ea0f8fecf79d46f7590a3ed608d20c42ccd31
Toshi <dtkellogg10@gmail.com>
2022-02-22 22:29:13 -0800

    Example app built.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <div className="cards__container">
+        <Card title="1" />
+        <Card title="2" />
+        <Card title="3" />
+        <Card title="4" />
+        <Card title="5" />
+        <Card title="6" />
+      </div>
     </div>
   );
 }
